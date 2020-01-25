296ed55b08f21e4f21b52f4b8d53c742030914b6
themeyer7 <themeyer7@byui.edu>
2020-01-25 00:40:42 -0900

    clean-up

--- js/date-js.js
+++
@@ -1,5 +0,0 @@
-/* js for assignment portal */
-
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-
-document.getElementById('currentdate').innerHTML = new Date().toLocaleDateString("en-US", options);
