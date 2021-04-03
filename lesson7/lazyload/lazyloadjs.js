c697eb9cc919afd30d9d6257a8c9bf8b6f311e7b
themeyer7 <themeyer7@byui.edu>
2021-04-02 16:49:12 -0800

    new intersection observer/gallery troubleshooting

--- lesson7/lazyload/lazyloadjs.js
+++ lesson7/lazyload/lazyloadjs.js
@@ -14,7 +14,7 @@ imagesToLoad.forEach((img) => {
 });
 
 //intersection observer
-
+/*
 if('IntersectionObserver' in window) {
   const observer = new IntersectionObserver((items, observer) => {
     items.forEach((item) => {
@@ -32,4 +32,21 @@ if('IntersectionObserver' in window) {
   imagesToLoad.forEach((img) => {
     loadImages(img);
   });
+}  */
+
+if(!!window.IntersectionObserver){console.log("I support Inserction Observer.")}
+
+
+// create a function
+let myObserver = new IntersectionObserver((myListA, myObserver) => {
+myListA.forEach(cupX => {
+if(cupX.isIntersecting){
+cupX.target.src = cupX.target.dataset.src;
+cupX.target.removeAttribute('data-src');
+myObserver.unobserve(cupX.target);
 }
+});
+}, {rootMargin: "0px 0px -10px 0px"});
+
+// make a list of all images with a data source and send that list to myObserver
+document.querySelectorAll('img[data-src]').forEach(img => {myObserver.observe(img) });
