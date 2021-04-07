74eeae0142f95a0475a930d70af84ce6c142ab4f
themeyer7 <themeyer7@byui.edu>
2021-04-07 02:38:58 -0800

    storm center done

---
+++ lesson8/stormcenter8/js/weatherscripts.js
@@ -0,0 +1,35 @@
+//toggle menu
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+
+//current date
+const option1 = {year: 'numeric'};
+
+const option2 = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+
+document.getElementById('currentyear').innerHTML = new Date().toLocaleDateString("en-US", option1);
+
+document.getElementById('currentdate').innerHTML = new Date().toLocaleDateString("en-US", option2);
+
+
+//special message
+const today = new Date();
+//console.log(today);
+
+const dayNumber = today.getDay();
+//console.log(dayNumber);
+
+const element = document.getElementById("message");
+
+if (dayNumber == 5) {
+  element.classList.add("showme");
+} else {
+  element.classList.add("hideme");
+}
+
+//adjust storm rating
+function adjustRating(rating) {
+  document.getElementById("ratingvalue").innerHTML = rating;
+}
